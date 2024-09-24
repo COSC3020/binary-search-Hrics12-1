c2f94234963c2cd50f77560180e63d1dd96fd778
Hrics12 <111898293+Hrics12@users.noreply.github.com>
2024-09-24 09:30:18 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,9 +1,25 @@
-function binarySearch(list, element) {
-    if (list.length ==0){
+function binarySearch(list,element){
+    if(list.length === 0){
         return -1;
     }
-    return -1;
-}
-let left = 0;
-let right = (list.length-1);
+    let left = 0; //first pointer
+    let right = list.length = -1; // second pointer
+    let mid = ((left+right)/2);// middle index
 
+    while (left<=right){
+        if (list[mid] === element) {
+        while (mid > 0 && list[mid - 1] === element){
+
+            mid--;
+        }
+        return mid;
+    }
+        else if (list[mid] < element)
+            left = mid + 1;
+        else
+            right = mid - 1;
+
+
+    }
+        return -1;
+}
