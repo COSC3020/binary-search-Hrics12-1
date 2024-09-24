cb0a0cae70b4f79447d616ed94a4ad995514967b
Hrics12 <111898293+Hrics12@users.noreply.github.com>
2024-09-24 09:45:51 -0600

    Update code.js

--- code.js
+++ code.js
@@ -3,11 +3,11 @@ function binarySearch(list,element){
         return -1;
     }
     let left = 0; //first pointer
-    let right = list.length = -1; // second pointer
+    let right = list.length -1; // second pointer
     
 
     while (left<=right){
-        let mid = Math.floor((left+right)/2);// middle index
+        mid = Math.floor((left+right)/2);// middle index
         if (list[mid] === element) {
         while (mid > 0 && list[mid - 1] === element){
 
